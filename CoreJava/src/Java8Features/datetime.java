75b896b52ccb6ea34813d7025077ae99b6e44d4a
anantsi-1103 <acera52024@gmail.com>
2026-02-21 11:21:58 +0530

    Add files via upload

---
+++ CoreJava/src/Java8Features/datetime.java
@@ -0,0 +1,86 @@
+package Java8Features;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class datetime {
+	public static void main(String[] args) {
+
+//		LocalDate ld = LocalDate.now();
+//		System.out.println(ld);
+//
+//		LocalDate custom = LocalDate.of(2025, 12, 25);
+//		System.out.println(custom);
+//
+////		Get parts 
+//		System.out.println(ld.getYear());
+//		System.out.println(ld.getMonth());
+//		System.out.println(ld.getDayOfMonth());
+
+//		LocalTime lt = LocalTime.now();
+//		System.out.println(lt);
+//
+//		LocalTime custom = LocalTime.of(10, 40);
+//		System.out.println(custom);
+//
+//		System.out.println(lt.getHour());
+//		System.out.println(lt.getMinute());
+
+//		LocalDateTime lcd = LocalDateTime.now();
+//		System.out.println(lcd);
+
+//		LocalDate ld = LocalDate.now();
+//		System.out.println(ld.plusDays(10));
+//		System.out.println(ld.minusMonths(2));
+//		System.out.println(ld.minusYears(2));
+//
+//		LocalTime lt = LocalTime.now();
+//
+//		System.out.println(lt);
+//		System.out.println(lt.plusHours(10));
+//		System.out.println(lt.plusSeconds(20));
+
+//		LocalDate l1 = LocalDate.of(2024, 1, 1);
+//		LocalDate l2 = LocalDate.of(2025, 1, 1);
+//		
+//		System.out.println(l1.isBefore(l2));
+//		System.out.println(l1.isAfter(l2));
+//		System.out.println(l1.isEqual(l2));
+
+//		Instant now = Instant.now();
+//		System.out.println(now);
+
+//		Date - Period
+//		Time - duration
+
+//		LocalDate l1 = LocalDate.of(2024, 1, 1);
+//		LocalDate l2 = LocalDate.of(2025, 5, 23);
+//
+//		Period p = Period.between(l1, l2);
+//
+//		System.out.println(p.getYears());
+//		System.out.println(p.getMonths());
+//		System.out.println(p.getDays());
+
+//		LocalTime lt1 = LocalTime.of(10, 40);
+//		LocalTime lt2 = LocalTime.of(18, 30);
+//		
+//		Duration d = Duration.between(lt1, lt2);
+//		
+//		System.out.println(d.toHours());
+//		System.out.println(d.toMinutes());
+
+//		LocalDate checkIn = LocalDate.of(2026, 2, 20);
+//		LocalDate checkOut = LocalDate.of(2026, 2, 25);
+//
+//		long totalDays = ChronoUnit.DAYS.between(checkIn, checkOut);
+//
+//		System.out.println("Total Stay Days: " + totalDays);
+//
+//		int pricePerDay = 2000;
+//		long totalAmount = totalDays * pricePerDay;
+//
+//		System.out.println("Total Bill: ₹" + totalAmount);
+
+	}
+}
